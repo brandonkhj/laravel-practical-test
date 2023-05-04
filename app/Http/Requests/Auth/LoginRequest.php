afcbe9051c24bf09bba6fa5dd886e890731895a1
Brandon Koh <brandonkhj@gmail.com>
2023-05-04 10:18:38 +0800

    run pint , cleanup

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -10,7 +10,7 @@ class LoginRequest extends ApiRequest
     {
         return [
             'email' => 'required|email|exists:users,email|min:6',
-            'password' => 'required|min:8'
+            'password' => 'required|min:8',
         ];
     }
 }
